1c5e40503536533f683e7d963a05294b2c19ff79
Ryder <fennera@student.okaloosaschools.com>
2026-03-05 15:59:20 +0000

    iuehgpiushrg

---
+++ main.py
@@ -0,0 +1,11 @@
+import os, pygame, shutil, tkinter as tk #import-inator-inize
+pygame.init()
+
+#ze launch-inator window, and its stuffs
+window_root = tk.Tk()
+window_root.title("launcher inator")
+window_root.geometry("300x200")
+window_root.resizable(False, False)
+window_root.configure(bg="#222")
+#show the window
+window_root.mainloop()
